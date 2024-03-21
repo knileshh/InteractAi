edb50b65ed777412f23ad8d6eba39626f8a642e1
knilesh9 <neutron_stays@protonmail.com>
2024-03-21 18:02:58 +0530

    Update Backend index.js removed some comments.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,7 @@
-//backend for the Ai
-
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import 'dotenv/config'
 
 
-// Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
 const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
@@ -14,7 +11,7 @@ async function run() {
     // For text-only input, use the gemini-pro model
     const model = genAI.getGenerativeModel({ model: "gemini-pro"});
   
-    const prompt = "Give me 1 quote from well know books and also give it's author and book name, format should be as a puzzle. Keep it short though"
+    const prompt = "Give me 1 quote from well know books and also give it's author and book name, format should be as a puzzle. Keep it short"
   
     const result = await model.generateContentStream([prompt]);
     // model.generateContentStream([prompt])
@@ -22,10 +19,6 @@ async function run() {
         const chunkText = chunk.text()
         console.log(chunkText)
     }
-
-    // const response = await result.response;
-    // const text = response.text();
-    // console.log(text);
   }
   
   run();
