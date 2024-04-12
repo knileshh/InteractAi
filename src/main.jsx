6fdeb1b34b05b0e0231ad4c211dde8d04ff26bc2
knilesh9 <neutron_stays@protonmail.com>
2024-04-12 21:33:54 +0530

    local search bar

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './index.css'
 
 import Layout from "./components/landingPage/Layout.jsx";
 import SignIn from "./components/landingPage/SignIn.jsx";
+import NewInput from "./components/landingPage/NewInput.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
               <Route path="/" element={<Layout />} />
               <Route path="/app" element={<App />} />
               <Route path="/signin" element={<SignIn/>} />
-
+              <Route path="/new-input" element={<NewInput/>} />
           </Routes>
       </BrowserRouter>
   </React.StrictMode>
