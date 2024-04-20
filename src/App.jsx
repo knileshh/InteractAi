52735cc57982e770b3ae11fcc68c35c4f1ec163f
knilesh9 <neutron_stays@protonmail.com>
2024-04-21 00:59:59 +0530

    Add Logo Link in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import OnlyLogo from "../interactAi/OnlyLogo.png"
 import './App.css';
+import { Link } from "react-router-dom"
 import ChatContainer from "./components/Ai Comps/ChatContainer.jsx";
 function App() {
     return (
         <div className="relative flex justify-center items-end min-h-screen bg-gray-100 pb-8">
-            <div className="absolute top-4 left-4 z-20">
+            <Link to="/" className="absolute top-4 left-4 z-20">
                 <img src={OnlyLogo} alt="Logo" className="h-8 w-auto opacity-50" />
-            </div>
+            </Link>
             <div className="absolute inset-0 flex justify-center items-center pointer-events-none z-0">
                 <h1 className="text-9xl font text-transparent bg-clip-text bg-gradient-to-r from-sky-200 to-red-200 animate-pulse-slow">Interact Ai</h1>
             </div>
