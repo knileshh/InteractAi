76d5071d2593c2bc77fc0d033961ee75085b589d
knilesh9 <neutron_stays@protonmail.com>
2024-04-21 00:27:57 +0530

    Final Touch done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import OnlyLogo from "../interactAi/OnlyLogo.png"
 import './App.css';
 import ChatContainer from "./components/Ai Comps/ChatContainer.jsx";
-
 function App() {
     return (
         <div className="relative flex justify-center items-end min-h-screen bg-gray-100 pb-8">
+            <div className="absolute top-4 left-4 z-20">
+                <img src={OnlyLogo} alt="Logo" className="h-8 w-auto opacity-50" />
+            </div>
             <div className="absolute inset-0 flex justify-center items-center pointer-events-none z-0">
                 <h1 className="text-9xl font text-transparent bg-clip-text bg-gradient-to-r from-sky-200 to-red-200 animate-pulse-slow">Interact Ai</h1>
             </div>
